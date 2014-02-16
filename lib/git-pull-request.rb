2628b2eb605fdd4623549c7f3ad42541e145891f
Dragos Miron <dragosmr@gmail.com>
2014-02-16 04:01:39 +0000

    added title and body

--- lib/git-pull-request.rb
+++ lib/git-pull-request.rb
@@ -2,24 +2,32 @@ require_relative "git-pull-request/version"
 require 'cgi'
 
 module Git
-	module PullRequest
+  module PullRequest
     extend self
     def request!
       branch = get_branch_name
       abort unless $?.success?
       
-      system("open https://www.github.com/#{repo_url}/compare/master...#{CGI.escape(branch.strip)}")
+      root_url = "https://www.github.com/#{repo_url}/compare/"
+      compared_branches = "master...#{CGI.escape(branch.strip)}"
+      options = ["pull_request[title]=#{CGI.escape(pr_title)}", "pull_request[body]=#{CGI.escape("Please give a description")}"]
+      system("open #{root_url}#{compared_branches}?#{options.join("&")}")
     end
 
     private
 
     def get_branch_name
-    	 `git rev-parse --abbrev-ref HEAD`
+       @branch_name ||= `git rev-parse --abbrev-ref HEAD`
     end
 
     def repo_url
-      `git config --get remote.origin.url`
+      @repo_url ||= `git config --get remote.origin.url`
         .sub(/.*github\.com[\/:]/,'').sub(/\.git$/,'').strip
     end
-	end
+
+    def pr_title
+      get_branch_name.gsub(/[^w]+/, " ").capitalize
+    end
+
+  end
 end
