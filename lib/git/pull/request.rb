1e2ca73400846b079db4f9f101a8fb15ac34824e
Dragos Miron <dragosmr@gmail.com>
2014-02-16 01:35:15 +0000

    first commit

---
+++ lib/git/pull/request.rb
@@ -0,0 +1,9 @@
+require "git/pull/request/version"
+
+module Git
+  module Pull
+    module Request
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/git/pull/request/version.rb
@@ -0,0 +1,7 @@
+module Git
+  module Pull
+    module Request
+      VERSION = "0.0.1"
+    end
+  end
+end
